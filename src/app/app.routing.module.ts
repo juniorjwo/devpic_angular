e105a7bb90fa42f950f94d64c5a4b9612062eb91
juniorjwo <juniorjwo@gmail.com>
2022-09-21 16:43:18 -0300

    Criado SignupComponent.

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,33 +7,39 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { SignInComponent } from './home/signin/signin.component';
 import { AuthGuard } from './core/auth/auth.guard';
+import { SignupComponent } from './home/signup/signup.component';
 
 const routes: Routes = [
-    { 
+    {
         path: '',
         component: SignInComponent,
         canActivate: [AuthGuard]
-    },    
-    { 
-        path: 'user/:userName', 
+    },
+    {
+      path: 'signup',
+      component: SignupComponent,
+
+  },
+    {
+        path: 'user/:userName',
         component: PhotoListComponent,
         resolve: {
             photos: PhotoListResolver
         }
     },
-    { 
-        path: 'p/add', 
-        component: PhotoFormComponent 
+    {
+        path: 'p/add',
+        component: PhotoFormComponent
     },
-    { 
-        path: '**', 
-        component: NotFoundComponent 
-    }  
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
 ];
 
 @NgModule({
-    imports: [ 
-        RouterModule.forRoot(routes) 
+    imports: [
+        RouterModule.forRoot(routes)
     ],
     exports: [ RouterModule ]
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule }  from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [ SignInComponent ],
-    imports: [ 
-        CommonModule, 
-        ReactiveFormsModule,
-        VMessageModule,
-        RouterModule
-    ]
+  declarations: [SignInComponent,
+    SignupComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    VMessageModule,
+    RouterModule,
+    FormsModule
+  ]
 })
 export class HomeModule { }
